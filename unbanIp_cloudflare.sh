b49f91f3fa9a7138154b8eb7c53606889d5835bb
EugenePikalov <eugene@pikalov.kiev.ua>
2019-11-19 13:29:21 +0200

    Update unbanIp_cloudflare.sh

--- unbanIp_cloudflare.sh
+++ unbanIp_cloudflare.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
+# Remove Cloudflare rules by name
+
+# Requirements:
 # yum install jq
 
 
 Email=""
 Key=""
-filterByNotesName="ddos2"
+filterByNotesName="ddos"
 
 ruleList=`curl -s -X GET "https://api.cloudflare.com/client/v4/user/firewall/access_rules/rules"   \
       -H "X-Auth-Key:$Key"    \
       -H "X-Auth-Email:$Email"  \
-      -H "Content-Type:application/json" | jq  '.result[] | select(.notes == "$filterByNotesName") | .id'`;
+      -H "Content-Type:application/json" | jq  ".result[] | select(.notes == \"$filterByNotesName\") | .id"`;
 
        for ruleid in $ruleList;
         do
